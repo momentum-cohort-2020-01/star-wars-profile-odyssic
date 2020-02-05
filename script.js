99b322904449c5450e505be9de69e43792e977db
odyssic <stevenbuddenjr@gmail.com>
2020-02-05 16:59:36 -0500

    working, order is odd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let starWarsData
+// let starWarsData
 
 const dataSection = document.querySelector('#container')
 
@@ -9,6 +9,13 @@ fetch('https://swapi.co/api/people/55/')
         return response.json()
     })
     .then(function(data) {
+        const img = document.createElement('img')
+        img.src = "adi-gallia-3_1539ac3a.webp"
+
+        dataSection.appendChild(img)
+
+        img.classList.add("br-100", "h4", "w4", "dib", "ba", "b--black-05", "pa2")
+
         const nameEl = document.createElement('h1')
         nameEl.classList.add("f3", "mb2")
             // nameEl.classList.add("mb2")
@@ -22,16 +29,21 @@ fetch('https://swapi.co/api/people/55/')
         const birthYear = document.createElement('p')
         birthYear.innerText = "Date of Birth: " + data.birth_year
         dataSection.appendChild(birthYear)
+        birthYear.classList.add("f5", "fw4", "gray", "mt0")
 
         const height = document.createElement('p')
         height.innerText = "Height: " +
             data.height
         dataSection.appendChild(height)
 
+        height.classList.add("f5", "fw4", "gray", "mt0")
+
         //starships = none
         const starShips = document.createElement('p')
         starShips.innerText = "Starships: None"
         dataSection.appendChild(starShips)
+
+        starShips.classList.add("f5", "fw4", "gray", "mt0")
 
 
 
@@ -49,6 +61,7 @@ fetch('https://swapi.co/api/species/23/')
     speciesName.innerText = "Species: " + data.name
 
     dataSection.appendChild(speciesName)
+    speciesName.classList.add("f5", "fw4", "gray", "mt0")
 
 });
 
@@ -64,6 +77,8 @@ fetch('https://swapi.co/api/planets/9/')
     homeWorld.innerText = "Homeworld: " + data.name
 
     dataSection.appendChild(homeWorld)
+
+    homeWorld.classList.add("f5", "fw4", "gray", "mt0")
 
 });
 
