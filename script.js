1c8a1c9a593d0dba04d566264d37a584172bd11a
odyssic <stevenbuddenjr@gmail.com>
2020-02-08 21:03:51 -0500

    fetch is working but list is not

--- script.js
+++ script.js
@@ -1,96 +1,89 @@
 // let starWarsData
 
-const dataSection = document.querySelector('#container')
+const dataSection = document.querySelector("#container");
 
-const profileList = document.createElement('ul')
-dataSection.classList.add("mw5", "center", "bg-white", "br3", "pa3", "pa4-ns", "mv3", "ba", "b--black-10")
+const profileList = document.createElement("ul");
+dataSection.classList.add(
+    "mw5",
+    "center",
+    "bg-white",
+    "br3",
+    "pa3",
+    "pa4-ns",
+    "mv3",
+    "ba",
+    "b--black-10"
+);
 
-fetch('https://swapi.co/api/people/55/')
+fetch("https://swapi.co/api/people/45/")
     .then(function(response) {
-        return response.json()
+        return response.json();
     })
     .then(function(data) {
-        const img = document.createElement('img')
-        img.src = "adi-gallia-3_1539ac3a.webp"
+        const img = document.createElement("img");
+        img.src = "adi-gallia-3_1539ac3a.webp";
 
-        dataSection.appendChild(img)
+        dataSection.appendChild(img);
 
-        img.classList.add("br-100", "h4", "w4", "dib", "ba", "b--black-05", "pa2")
+        img.classList.add("br-100", "h4", "w4", "dib", "ba", "b--black-05", "pa2");
 
-        const nameEl = document.createElement('h1')
-        nameEl.classList.add("f3", "mb2")
-            // nameEl.classList.add("mb2")
+        const nameEl = document.createElement("h1");
+        nameEl.classList.add("f3", "mb2");
+        // nameEl.classList.add("mb2")
 
-        nameEl.innerText = data.name
+        nameEl.innerText = data.name;
 
-        dataSection.appendChild(nameEl)
+        dataSection.appendChild(nameEl);
 
         // dataSection.appendChild(homeWorld)
 
+        dataSection.appendChild(profileList);
 
+        const birthYear = document.createElement("li");
+        birthYear.innerText = "Date of Birth: " + data.birth_year;
+        profileList.appendChild(birthYear);
+        birthYear.classList.add("f5", "fw4", "gray", "mt0");
 
-        dataSection.appendChild(profileList)
+        const height = document.createElement("li");
+        height.innerText = "Height: " + data.height;
+        profileList.appendChild(height);
 
-        const birthYear = document.createElement('li')
-        birthYear.innerText = "Date of Birth: " + data.birth_year
-        profileList.appendChild(birthYear)
-        birthYear.classList.add("f5", "fw4", "gray", "mt0")
-
-        const height = document.createElement('li')
-        height.innerText = "Height: " +
-            data.height
-        profileList.appendChild(height)
-
-        height.classList.add("f5", "fw4", "gray", "mt0")
+        height.classList.add("f5", "fw4", "gray", "mt0");
 
         //starships = none
-        const starShips = document.createElement('li')
-        starShips.innerText = "Starships: None"
-        profileList.appendChild(starShips)
+        const starShips = document.createElement("li");
+        starShips.innerText = "Starships: None";
+        profileList.appendChild(starShips);
 
-        starShips.classList.add("f5", "fw4", "gray", "mt0")
-
+        starShips.classList.add("f5", "fw4", "gray", "mt0");
     });
 
-fetch('https://swapi.co/api/species/23/')
+fetch("https://swapi.co/api/species/23/")
     .then(function(response) {
-        return response.json()
+        return response.json();
     })
 
 .then(function(data) {
+    let speciesName = document.createElement("li");
 
-    let speciesName = document.createElement('li')
+    speciesName.innerText = "Species: " + data.name;
 
-    speciesName.innerText = "Species: " + data.name
+    profileList.appendChild(speciesName);
 
-    profileList.appendChild(speciesName)
-
-    speciesName.classList.add("f5", "fw4", "gray", "mt0")
-
+    speciesName.classList.add("f5", "fw4", "gray", "mt0");
 });
 
-fetch('https://swapi.co/api/planets/9/')
+fetch("https://swapi.co/api/planets/9/")
     .then(function(response) {
-        return response.json()
+        return response.json();
     })
 
 .then(function(data) {
+    let homeWorld = document.createElement("li");
 
-    let homeWorld = document.createElement('li')
+    homeWorld.innerText = "Homeworld: " + data.name;
 
-    homeWorld.innerText = "Homeworld: " + data.name
+    profileList.appendChild(homeWorld);
 
-    profileList.appendChild(homeWorld)
-
-    homeWorld.classList.add("f5", "fw4", "gray", "mt0")
-
+    homeWorld.classList.add("f5", "fw4", "gray", "mt0");
 });
-
-
-// 
-// example fetch('https://api.github.com/orgs/momentum-cohort-2020-01')
-//     .then(function(response) {
-//         return response.json()
-//     })
-//     .then(function(data) {
-//         const newEl = document.createElement('h2'
